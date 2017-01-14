44ebb9e055f64c2327e15bb2f815f0c741920efc
pawandeep2007 <pawandeep2007@gmail.com>
2017-01-15 00:45:57 +0530

    Login POST method

--- index.js
+++ index.js
@@ -39,134 +39,158 @@ var test = function () {
 };
 
 var App = function () {
-    "use strict";
+        "use strict";
 
-    var self = this;
+        var self = this;
 
-    self.setupVariables = function () {
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port = process.env.OPENSHIFT_NODE_PORT || 8081;
+        self.setupVariables = function () {
+            self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+            self.port = process.env.OPENSHIFT_NODE_PORT || 8081;
 
-        if (typeof self.ipaddress === 'undefined') {
-            console.log('No OPENSHIFT_NODEJS_IP\nUsing 127.0.0.1');
-            self.ipaddress = '127.0.0.1';
-        }
-    };
-
-    self.populateCache = function () {
-        if (typeof self.zcache === 'undefined') {
-            self.zcache = {
-                'index.html': ''
-            };
+            if (typeof self.ipaddress === 'undefined') {
+                console.log('No OPENSHIFT_NODEJS_IP\nUsing 127.0.0.1');
+                self.ipaddress = '127.0.0.1';
+            }
         };
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
-    };
 
-    self.cache_get = function (key) {
-        return self.zcache[key];
-    };
+        self.populateCache = function () {
+            if (typeof self.zcache === 'undefined') {
+                self.zcache = {
+                    'index.html': ''
+                };
+            };
+            self.zcache['index.html'] = fs.readFileSync('./index.html');
+        };
 
-    self.terminator = function (sig) {
-        if (typeof sig === 'string') {
-            console.log('%s Received %s - terminating app...', Date(Date.now()), sig);
-            process.exit(1);
+        self.cache_get = function (key) {
+            return self.zcache[key];
+        };
+
+        self.terminator = function (sig) {
+            if (typeof sig === 'string') {
+                console.log('%s Received %s - terminating app...', Date(Date.now()), sig);
+                process.exit(1);
+            }
+            console.log('%s: Node server stopped.', Date(Date.now()));
         }
-        console.log('%s: Node server stopped.', Date(Date.now()));
-    }
 
-    self.setupTerminationHandlers = function () {
-        process.on('exit', function () {
-            self.terminator();
-        });
+        self.setupTerminationHandlers = function () {
+            process.on('exit', function () {
+                self.terminator();
+            });
 
         [
             'SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT', 'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
         ].forEach(function (element, index, array) {
-            process.on(element, function () {
-                self.terminator(element);
+                process.on(element, function () {
+                    self.terminator(element);
+                });
             });
-        });
-    };
-
-    self.createRoutes = function () {
-        self.routes = {};
-
-        self.routes['/'] = function (req, res) {
-            res.setHeader('Content-type', 'text/html');
-            res.send(self.cache_get('index.html'));
         };
-    };
 
-    self.initializeServer = function () {
-        var r;
-        self.createRoutes();
-        self.app = express();
+        self.createRoutes = function () {
+            self.routes = {};
 
-        self.app.use(cookieParser());
+            self.routes['/'] = function (req, res) {
+                res.setHeader('Content-type', 'text/html');
+                res.send(self.cache_get('index.html'));
+            };
+        };
 
-        //        self.app.set('trust proxy', 1);
-        self.app.use(session({
-            secret: Config.secret,
-            key: Config.key,
-            resave: true,
-            saveUninitialized: true
-        }));
+        self.initializeServer = function () {
+            var r;
+            self.createRoutes();
+            self.app = express();
 
-        self.app.use(bodyParser.json());
+            self.app.use(cookieParser());
 
-        for (r in self.routes) {
-            if (self.routes.hasOwnProperty(r)) {
-                self.app.get(r, self.routes[r]);
+            //        self.app.set('trust proxy', 1);
+            self.app.use(session({
+                secret: Config.secret,
+                key: Config.key,
+                resave: true,
+                saveUninitialized: true
+            }));
+
+            self.app.use(bodyParser.json());
+
+            for (r in self.routes) {
+                if (self.routes.hasOwnProperty(r)) {
+                    self.app.get(r, self.routes[r]);
+                }
             }
-        }
 
-        self.app.post('/register', function (req, res) {
-            console.log('User Register Post\n\n');
-            var user = req.body;
-            req.session.token = Math.floor(20 * Math.random())
-            if (user.is == 0) {
-                if (Student.findByRollNumber(newUser.rollNumber)) {
-                    var newStudent = new Student(user);
-                    newStudent.save().then(function () {
+            self.app.post('/register', function (req, res) {
+                console.log('User Register Post\n\n');
+                var user = req.body;
+                req.session.token = Math.floor(20 * Math.random())
+                if (user.is == 0) {
+                    if (Student.findByRollNumber(user.rollNumber)) {
+                        var newStudent = new Student(user);
+                        newStudent.save().then(function () {
+                            res.json(req.session);
+                        }).catch(function (err) {
+                            console.log('Error saving model: ');
+                            console.log(err);
+                            res.status(500).send(err);
+                        });
+                    }
+                } else if (user.is == 1) {
+                    //For teacher
+                } else if (user.is == 2) {
+                    //For Admin
+                } else {
+                    //Invalid
+                }
+            });
+
+            self.app.post('/login', function (req, res) {
+                console.log('Login POST method');
+                var user = req.body;
+                if (user.is == 0) {
+                    Student.find({
+                        rollNumber: user.rollNumber,
+                        password: user.password
+                    }).then(function(data) {
+                        req.session.name = user.name;
                         res.json(req.session);
-                    }).catch(function (err) {
-                        console.log('Error saving model: ');
+                    }).catch(function(err) {
+                        console.log('Invalid credentials: ');
                         console.log(err);
                         res.status(500).send(err);
                     });
+                } else if(user.is == 1) {
+                    //Login teacher
+                } else if(user.is == 2) {
+                    //Login admin
+                } else {
+                    //Invalid
                 }
-            } else if (user.is == 1) {
-                //For teacher
-            } else if (user.is == 2) {
-                //For Admin
-            } else {
-                //Invalid
-            }
-        });
-    };
+            });
 
-    self.initialize = function () {
-        self.setupVariables();
-        self.populateCache();
-        self.setupTerminationHandlers();
 
-        self.initializeServer();
-    };
+            self.initialize = function () {
+                self.setupVariables();
+                self.populateCache();
+                self.setupTerminationHandlers();
 
-    self.start = function () {
-        self.app.listen(self.port, self.ipaddress, function () {
-            console.log('%s: Node server started on %s:%d...', Date(Date.now()), self.ipaddress, self.port);
-        });
-    };
+                self.initializeServer();
+            };
 
-    test();
-};
+            self.start = function () {
+                self.app.listen(self.port, self.ipaddress, function () {
+                    console.log('%s: Node server started on %s:%d...', Date(Date.now()), self.ipaddress, self.port);
+                });
+            };
 
-var start = function () {
-    var zApp = new App();
-    zApp.initialize();
-    zApp.start();
-}
+            test();
+        };
 
-db.on('error', console.error.bind(console, 'connection error: '));
-db.once('open', start);
+        var start = function () {
+            var zApp = new App();
+            zApp.initialize();
+            zApp.start();
+        }
+
+        db.on('error', console.error.bind(console, 'connection error: '));
+        db.once('open', start);
